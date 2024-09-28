24eccc44dff4d6994de104f0fe90cc9388e7794f
Ben Wang <benwhz@foxmail.com>
2024-09-28 09:19:06 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # RAG
- build a simple RAG pipeline from scratch
+
+We built a simple RAG (Retrieval-augmented generation) pipeline from scratch, using the following open sources models on [huggingface](https://huggingface.co/):
+- Embedding: [FlagEmbedding](https://huggingface.co/BAAI/bge-small-zh-v1.5)
+- LLM: [Qwen2.5](https://huggingface.co/Qwen/Qwen2.5-72B-Instruct)
+
+**RAG modules**:
+1. SimpeFileReader: just read a text file
+1. SentenceSplitter: split the docs with specified chunk size and overlap.
+1. VectorStoreIndex: build a vector store with the chunked nodes and create the index.
+
+**Work steps**:
+1. read the text file.
+1. normalize the file content.
+1. split the content into chunks.
+1. build the vector store with the chunks.
+1. define the question.
+1. query the context of this quesion from vector store.
+1. format the prompt with the question and it's context.
+1. predict the prompt with the LLM.
+1. get the answer.
+
+Tested and run under Python 3.12.6, the following packages are also required
+- [sentence-transformers 3.1.1](https://www.sbert.net/)
+- [gradio-client 1.3.0](https://www.gradio.app/guides/getting-started-with-the-python-client)
+
+> The test text documents were obtained from Google search on the Internet.
--- rag.py
+++ rag.py
@@ -1,16 +1,34 @@
+'''
+Copyright (c) 2024 Ben Wang, Email: benwhz@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
 from typing import Any
 from sentence_transformers import SentenceTransformer
-from gradio_client import Client, handle_file
-
-# embedding model    
-embedding = SentenceTransformer('BAAI/bge-small-zh-v1.5')
+from gradio_client import Client
 
 # file reader
 class SimpleFileReader():
     def __init__(self, filename) -> None:
         self.fn =filename
     
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
+    def __call__(self) -> Any:
         with open(self.fn, 'r', encoding='utf-8') as file:
             return file.read()
     
@@ -23,15 +41,13 @@ class SentenceSplitter():
     def split(self, docs):
         splits = []
         start = 0
-        exit = False
         while True:
             end = start + self.chunk
             if end >= len(docs):
                 end = len(docs)
-                exit = True
             splits.append(docs[start:end])
             start = end - self.overlap
-            if exit:
+            if end == len(docs):
                 break
         return splits
 
@@ -64,35 +80,35 @@ class VectorStoreIndex():
         self.vectors.clear()
     
 docs = SimpleFileReader('../dataset/news.txt')()
+# normalize
+docs = docs.replace("\r\n",'')
+docs = docs.replace("\n",'')
+
 splitter = SentenceSplitter()
 chunks = splitter.split(docs)
+
+# embedding model    
+embedding = SentenceTransformer('BAAI/bge-small-zh-v1.5')
 index = VectorStoreIndex(embedding)(chunks)
 
 question = "Who will be the new Prime Minister of Japan?"
 _, context = index.query(question)
-# print(context)
 
 # LLM from Huggingface
 client = Client("Qwen/Qwen2.5")
 
 # prompt with context.
 query_text = "Consider the following background: " + context + ", Please answer this question: " + question
+print("Q: ", question)
 
 result = client.predict(
 		query=query_text,
 		history=[],
-		system="You are an AI robot. Please answer the questions according to the background information provided.",
+		system="You are an AI robot. Please answer the questions according to the background information provided simply.",
 		radio='72B',
 		api_name="/model_chat"
 )
 
-print(result)
+answer_text = result[1][0][1]['text']
+print("A: ", answer_text)
 
-exit()
-
-#print(len(splits), splits[-2], splits[-1])
-embeddings_2 = embedding.encode(chunks[0])
-embeddings_1 = embedding.encode(chunks[-1])
-print(type(embeddings_1), embeddings_1.shape, embeddings_2.shape)
-similarity = embeddings_1 @ embeddings_2.T
-print(similarity)
